c4c9cb68a97ed6febc93c0f3409a88fdaa08262b
Raza <rsc1099@gmail.com>
2025-12-30 01:38:19 +0000

    fix api deploy

--- server/app.js
+++ server/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('./logger')
 const clicheRouter = require('./routers/cliches');
 
 const app = express();
 app.use(cors());
+app.use(logger);
 app.use(express.json());
 
 app.use('/cliches', clicheRouter);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    title: "Cliches",
+    description: "The gigs await..."
+  })
+})
 
 module.exports = app
