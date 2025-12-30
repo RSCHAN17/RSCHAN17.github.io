1cda67f49b5f38ca18b101d12e98e9eff34c900a
Raza <rsc1099@gmail.com>
2025-12-30 17:41:53 +0000

    started create frontend

--- gb1.js
+++ gb1.js
@@ -1,4 +1,37 @@
-let tropes = ['Someone turned around whole song','Facetime','Weird device','No clue what\'s going on','On shoulders','Crowd surfer','Lookalike','Weird interaction with stranger','Getting / Viewing beered','Clothing Chucked','Lighter','Someone you actually know','Premature moshing','Blinding Lights','Confetti','Cardboard sign','Screaming the whole time','Mismatched acts','Uncomfortably long encore wait','Random single living their best life','Music industry/ political/ religious speech','Football shirt','Movie/TV shirt','This is our "last song"','Artist "can\'t believe they\'re there"','Missing shoe in mosh','Phone torch looking for something','S/O dragged along but having an amazing time','Getting a ticket day of','Scary fan (way too invested)','Artist restarting the song','The artist\'s music being blasted on route back','Someone dressed like the artist','Fake merch','Queuing up for over 3 hrs outside','Setlist / drumstick being chucked into the crowd','Surprise guest','Snapchat','Getting Merch']
+let completeList = []
+const start = fetchAllCliches()
+let bingoBoard = []
+const clicheList = document.querySelector('#allTropes')
+
+const clicheForm = document.querySelector('#clicheForm')
+clicheForm.addEventListener("submit",submitCliche)
+
+async function submitCliche() {
+    
+}
+
+
+let idList = []
+for (let i=1; i<25; i++) {
+    idList.push('a'+i.toString())
+}
+
+async function fetchAllCliches() {
+    try {
+        const response = await fetch(`https://rschan17-github-io.onrender.com/cliches/all`);
+        const data = await response.json()
+        for (let i=1; i<data.length; i++) {
+            completeList.push(data[i]["cliche"])
+            const li = clicheList.appendChild(document.createElement("li"))
+            li.innerHTML=data[i]["cliche"];
+        };
+        bingoBoard=shuffle(completeList).slice(0,24)
+        idList.forEach(fillBoard)
+    } catch (err) {
+        return new Error("Failed to fetch cliches")
+    }
+}
+
 const backup = tropes
 
 function shuffle(array) {
@@ -15,19 +48,15 @@ function shuffle(array) {
     return array;
 }
 
-const bingoBoard = shuffle(tropes).slice(0,24)
 
 
-let idList = []
-for (let i=1; i<25; i++) {
-    idList.push('a'+i.toString())
-}
+
 
 function fillBoard(id) {
     document.getElementById(id).innerHTML = bingoBoard[parseInt(id.slice(1))-1];
 }
 
-idList.forEach(fillBoard);
+
 
 function pressed(id) {
     if(document.getElementById(id).style.backgroundColor == 'indigo'){
