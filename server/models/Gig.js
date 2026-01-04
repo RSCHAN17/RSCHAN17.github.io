047d7544f13f502962f5c6f06cded72b766cb364
Raza <rsc1099@gmail.com>
2026-01-04 20:47:46 +0000

    added type to Gig Model

--- server/models/Gig.js
+++ server/models/Gig.js
@@ -5,10 +5,10 @@ class Gig {
         this.gig_id=gig_id;
         this.date=date;
         this.name=name;
-        this.type;
+        this.type=type;
     }
     static async getAll() {
-    const response = await db.query("SELECT * FROM gigs;");
+    const response = await db.query("SELECT gig_id, substring(cast(date as varchar(10),1,10) as date, name, type FROM gigs;");
     if (response.rows.length === 0) {
         throw new Error("No gigs available.")
     }
@@ -16,7 +16,7 @@ class Gig {
     }
 
     static async getAllFestivals() {
-    const response = await db.query("SELECT * FROM gigs WHERE type='festival';");
+    const response = await db.query("SELECT gig_id, substring(cast(date as varchar(10),1,10) as date, name, type FROM gigs WHERE type='festival';");
     if (response.rows.length === 0) {
         throw new Error("No festivals available.")
     }
